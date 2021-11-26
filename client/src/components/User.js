48319d3fec58cd3e2673f92c9fcb074eed722e95
othmane <othman-19@hotmail.fr>
2021-11-26 20:40:27 +0100

    setup User and UserList componennts

---
+++ client/src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const User = () => (
+  <div>
+    <p> This is the user page</p>
+  </div>
+);
+
+export default User;
---
+++ client/src/components/UsersList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Users = () => (
+  <div>
+    <p> This is the users list page</p>
+  </div>
+);
+
+export default Users;
