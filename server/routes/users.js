68a4cce93a8278f7bc3aba65e7d57f82aa9e3cf4
othmane <othman-19@hotmail.fr>
2021-11-26 09:30:24 +0100

    add user signup route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,43 @@
+/* eslint-disable consistent-return */
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config/config');
+const User = require('../models/User');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.post('/signup', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user) {
+        return res.status(409).json({
+          message: 'Mail exists',
+        });
+      }
+
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        }
+        const user = new User({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: hash,
+        });
+        user.save()
+          .then(result => res.status(201).json({
+            message: 'User created',
+          }))
+          .catch(err => res.status(500).json({
+            error: err,
+          }));
+      });
+    });
 });
 
 module.exports = router;
