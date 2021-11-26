e060260235331164f228e8d9eecb4a559e3a8297
othmane <othman-19@hotmail.fr>
2021-11-26 09:14:43 +0100

    set user route middleware

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.use(cors({
 }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
