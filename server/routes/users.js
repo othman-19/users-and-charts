4310d4ae7893406cd43d84f2453627d4a6bba1b2
othmane <othman-19@hotmail.fr>
2021-11-26 09:26:28 +0100

    install bcryptjs

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
