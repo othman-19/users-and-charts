96db18749919fdfa583771c181339f80363f8b96
othmane <othman-19@hotmail.fr>
2021-11-26 09:27:39 +0100

    require jwt and jwtsecret

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/config');
 
 const router = express.Router();
 
