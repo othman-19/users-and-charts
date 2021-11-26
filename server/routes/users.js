1e7ae6db869836a914ba0dad91cdbb72b083fa1a
othmane <othman-19@hotmail.fr>
2021-11-26 09:31:41 +0100

    add user login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -40,4 +40,41 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        });
+      }
+      bcrypt.compare(
+        req.body.password,
+        user.password,
+        (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: 'Auth failed',
+            });
+          }
+          if (result) {
+            const token = jwt.sign(
+              { email: user.email, userId: user._id },
+              jwtSecret,
+              { expiresIn: '1h' },
+            );
+            return res.status(200).json({
+              message: 'Auth successful',
+              token,
+            });
+          }
+        },
+      );
+    })
+    .catch(err => res.status(500).json({
+      error: err,
+    }));
+});
+
 module.exports = router;
