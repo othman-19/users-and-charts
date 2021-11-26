89e5157427ffaf7ce8822dfa35bde9eed3f1c7e1
othmane <othman-19@hotmail.fr>
2021-11-26 16:51:01 +0100

    add UI routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
 
 const App = function () {
   return (
-    <div className="App" />
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+        <Switch>
+          <Route exact path="/users" component={UsersList}/>
+        </Switch>
+        <Switch>
+          <Route exact path="/users/id" component={User}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 };
 
