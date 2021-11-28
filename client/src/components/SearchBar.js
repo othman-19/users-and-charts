2180d59a8a31e6a1e9ee90e2851d2a780075f8b0
othmane <othman-19@hotmail.fr>
2021-11-28 09:28:00 +0100

    add styles to serch input

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,13 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 
 const SearchBar = ({ placeholder, handleChange }) => (
-  <input
-    type="search"
-    className="search"
-    placeholder={placeholder}
-    onChange={handleChange}
-  />
+  <Box
+    component="form"
+    sx={{
+      '& > :not(style)': { m: 1, width: '50ch', marginTop: '2rem' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <TextField
+      type="search"
+      className="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+      id="filled-basic"
+      label="First Name"
+      variant="filled"
+    />
+  </Box>
 );
 
 export default SearchBar;
