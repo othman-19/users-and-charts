2b6dbe3ddf4eb12bbc315d258b9ed86e6f5f1e19
othmane <othman-19@hotmail.fr>
2021-11-28 09:03:42 +0100

    add styles to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 
-const Navbar = () => (
-  <>
-    <Link to="/">
-      Home
-    </Link>
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: 'flex',
+  },
+  logo: {
+    flexGrow: '1',
+    cursor: 'pointer',
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white',
+    fontSize: '20px',
+    marginLeft: theme.spacing(25),
+    '&:hover': {
+      color: 'yellow',
+      borderBottom: '1px solid white',
+    },
+  },
+}));
 
-    <Link to="/users">
-      Users List
-    </Link>
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Navbar
+        </Typography>
+        <div className={classes.navlinks}>
+          <Link to="/" className={classes.link}>
+            Home
+          </Link>
 
-    <Link to="/about">
-      About
-    </Link>
-  </>
-);
+          <Link to="/users" className={classes.link}>
+            Users List
+          </Link>
+
+          <Link to="/about" className={classes.link}>
+            About
+          </Link>
+
+          <Link to="/contact" className={classes.link}>
+            Contact
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
