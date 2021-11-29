78db66349693dcde3aa41d8ab8e6e81f495b12c1
othmane <othman-19@hotmail.fr>
2021-11-29 17:01:03 +0100

    change error handler

--- server/app.js
+++ server/app.js
@@ -76,14 +76,18 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((error, req, res, next) => {
+  if (req.app.get('env') === 'production') {
+    res.status(500);
+    res.render('error', { message: '500: Internal Server Error', error });
+  } else {
+    // set locals, only providing error in development
+    res.message = error.message;
+    res.error = error;
+    // render the error page
+    res.status(error.status || 500);
+    res.send(error);
+  }
 });
 
 module.exports = app;
